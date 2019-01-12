1e12584ff625b8219b6dbcb2604e38b30f5ff0df
Eva Killenberg <evakill@seas.upenn.edu>
2019-01-12 08:02:40 -0500

    Fixed linkedin link

--- components/Footer.js
+++ components/Footer.js
@@ -21,8 +21,9 @@ class Footer extends React.Component {
         <div className="hero is-small" style={{marginTop: '4rem', backgroundColor: "#f5f5f5"}}>
           <div className="hero-body">
             <div className="is-flex" style={{flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
+            <em className="subtitle is-size-7" style={{color: "black", padding: 0, margin: 0 }}> site + graphics by</em>
               <b className="is-size-5" style={{color: "black"}}> Eva Killenberg </b>
-              <div style={{display:"flex", justifyContent:"center"}}>
+              <div style={{display:"flex", justifyContent:"center", padding: 0, margin: 0}}>
                 <Square
                 pose={this.state.h1 ? "hovered" : "idle"}
                 onMouseEnter={() => this.setState({ h1: true })}
@@ -37,7 +38,7 @@ class Footer extends React.Component {
                 pose={this.state.h2 ? "hovered" : "idle"}
                 onMouseEnter={() => this.setState({ h2: true })}
                 onMouseLeave={() => this.setState({ h2: false })}>
-                  <a href="https://www.linkedin.com/in/eva-k-752386b4/">
+                  <a href="https://www.linkedin.com/in/evakill/">
                     <span className="icon" style={{color: "black"}}>
                       <i className="fab fa-linkedin"></i>
                     </span>
@@ -54,7 +55,7 @@ class Footer extends React.Component {
                   </a>
                 </Square>
               </div>
-              <em className="subtitle is-size-7" style={{color: "black"}}> 2018 </em>
+              <em className="subtitle is-size-7" style={{color: "black", padding: 0, margin: 0 }}>2019</em>
             </div>
           </div>
         </div>
