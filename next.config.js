14e7be3fc12fb32563d740eb871714e4c7ea3ed5
Eva Killenberg <>
2025-08-30 20:25:47 -0400

    next config export

--- next.config.js
+++ next.config.js
@@ -4,10 +4,8 @@ const nextConfig = {
     images: {
         unoptimized: true
     },
-    // Set basePath and assetPrefix for GitHub Pages
-    // Replace 'your-repo-name' with your actual repository name
     basePath: process.env.NODE_ENV === 'production' ? '/evakillenbergv20/' : '',
     assetPrefix: process.env.NODE_ENV === 'production' ? '/evakillenbergv20/' : '',
-}
+};
 
-export default nextConfig;
+module.exports = nextConfig;
