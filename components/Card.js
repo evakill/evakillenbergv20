ccd0a2dd50d2f9dc6b0da345017b55201d0bf13f
Eva Killenberg <>
2025-08-30 22:12:01 -0400

    use asset path in prod

--- components/Card.js
+++ components/Card.js
@@ -22,7 +22,7 @@ const Img = s.img`
 `
 
 class Card extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       modal: "modal",
@@ -31,27 +31,27 @@ class Card extends React.Component {
 
   modalToggle = (e) => {
     var newClass = (this.state.modal === "modal") ? "modal is-active" : "modal";
-    this.setState({modalClass: newClass});
+    this.setState({ modalClass: newClass });
   }
 
   render() {
-    return(
+    return (
       <Container className="columns is-flex">
         <div className="column is-2 is-2-mobile is-flex">
-          <Img src={this.props.src}/>
+          <Img src={process.env.NODE_ENV === 'production' ? '/evakillenbergv20' : '' + this.props.src} />
         </div>
         <div className="column is-10">
           <p className="is-size-5 is-size-6-mobile" style={{ fontWeight: 500 }}>{this.props.title}</p>
-          { this.props.role ? <p className="is-size-6 is-size-7-mobile" style={{color: "gray"}}>{this.props.role}</p> : '' }
+          {this.props.role ? <p className="is-size-6 is-size-7-mobile" style={{ color: "gray" }}>{this.props.role}</p> : ''}
           <p className="is-size-6">{this.props.desc} </p>
           <div className="container is-flex">
-            { this.props.link ? (
+            {this.props.link ? (
               <a target="_blank" href={this.props.link == "Coming Soon!" ? "" : this.props.link}>
                 <Link className="tag has-text-grey is-size-7">
                   {this.props.link}
                 </Link>
               </a>
-              ) : <div></div> }
+            ) : <div></div>}
           </div>
         </div>
       </Container>
--- components/Header.js
+++ components/Header.js
@@ -8,37 +8,37 @@ const Container = s.div`
   padding: 2rem 0;
 `
 
-export default function Header () {
-    return (
-        <Container>
-          <Head>
-            <meta charset="utf-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <title>Eva Killenberg</title>
-            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css" />
-            <script defer src="https://use.fontawesome.com/releases/v5.14.0/js/all.js"></script>
-            <link rel="preconnect" href="https://fonts.gstatic.com" />
-            <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;600&display=swap" rel="stylesheet" />
-          </Head>
-          <div>
-            <img src="/static/img/header.png" style={{ padding:'3rem 0rem' }}/>
-          </div>
-          <p className="is-size-4 is-size-6-mobile">
-            And this is my website.
-            <br/>
-            <br/>
-            <br/>
-            I'm a builder, storyteller, and adventurer. I strive for balance and growth.
-            I seek opportunities to lift others up. I'm happiest when learning, exploring, and connecting with people.
-            <br/>
-            <br/>
-            I'm originally from Little Compton, Rhode Island, but I have found homes in
-            Providence, Newport, Chapel Hill, Philadelphia, San Francisco, Raleigh, Dunedin, and Thames.
-            I'm currently living and learning at the University of Pennsylvania, with a major in
-            Computer Science and a minor in Urban Studies.
-            <br/>
-            <br/>
-          </p>
-        </Container>
-    )
+export default function Header() {
+  return (
+    <Container>
+      <Head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Eva Killenberg</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css" />
+        <script defer src="https://use.fontawesome.com/releases/v5.14.0/js/all.js"></script>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;600&display=swap" rel="stylesheet" />
+      </Head>
+      <div>
+        <img src={process.env.NODE_ENV === 'production' ? '/evakillenbergv20' : '' + "/static/img/header.png"} style={{ padding: '3rem 0rem' }} />
+      </div>
+      <p className="is-size-4 is-size-6-mobile">
+        And this is my website.
+        <br />
+        <br />
+        <br />
+        I'm a builder, storyteller, and adventurer. I strive for balance and growth.
+        I seek opportunities to lift others up. I'm happiest when learning, exploring, and connecting with people.
+        <br />
+        <br />
+        I'm originally from Little Compton, Rhode Island, but I have found homes in
+        Providence, Newport, Chapel Hill, Philadelphia, San Francisco, Raleigh, Dunedin, and Thames.
+        I'm currently living and learning at the University of Pennsylvania, with a major in
+        Computer Science and a minor in Urban Studies.
+        <br />
+        <br />
+      </p>
+    </Container>
+  )
 }
