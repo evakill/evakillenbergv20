918de87d99cbf80e70051bd15428e49e3a526011
Eva Killenberg <>
2025-08-30 21:53:21 -0400

    remove backslash

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ const nextConfig = {
     images: {
         unoptimized: true
     },
-    basePath: process.env.NODE_ENV === 'production' ? '/evakillenbergv20/' : '',
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/evakillenbergv20/' : '',
+    basePath: process.env.NODE_ENV === 'production' ? '/evakillenbergv20' : '',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/evakillenbergv20' : '',
 };
 
 module.exports = nextConfig;
